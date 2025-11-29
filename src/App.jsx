0a5145c61f71f0d0d5706b143c64c07bce125433
axle29 <axlefredi@gmail.com>
2025-11-29 22:41:25 +0100

    second  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Quiz from "./Quiz/Quiz.jsx"
 
 function App() {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace REPO_NAME with your GitHub repository name
 export default defineConfig({
+  base: '/quizappreact/', 
   plugins: [react()],
-})
+});
