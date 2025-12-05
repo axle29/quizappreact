266a8e6eb6a759d48d7ad2b1e17d2ea442641b3b
axle29 <axlefredi@gmail.com>
2025-12-05 17:41:11 +0100

    msg fix

--- src/Congrats/Congrats.jsx
+++ src/Congrats/Congrats.jsx
@@ -4,7 +4,7 @@ function Congrats(props){
 
     return(
     <div className={styles.quizwrapper}>
-        <img src="/resources/congrats.png"></img>
+        <img src="resources/congrats.png"></img>
         <h1>Congrats! You completed the quiz.</h1>
         <h2>You answer {props.currentScore}/10 correctly.</h2>
         <button onClick={props.onPlayAgain}>Play again</button>
